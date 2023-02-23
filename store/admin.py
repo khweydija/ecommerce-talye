5e18c9fe8914756fab756ef594d22f7d1f021b7d
khweydija <khweydija12@gmail.com>
2023-02-23 03:00:09 +0100

    thanye

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,30 @@
+
 from django.contrib import admin
+from .models import Category, Product, Commande
+
+admin.site.site_header = "E-commerce"
+admin.site.site_title = "Tsew9"
+admin.site.index_title = "Manageur"
+
+
+
+class AdminCategorie(admin.ModelAdmin):
+    list_display = ('name', 'date_added')
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category', 'date_added')
+    search_fields = ('title',) 
+    list_editable = ('price',)
+
+class AdminCommande(admin.ModelAdmin):
+    list_display = ('items','nom','email','address', 'ville', 'pays','total', 'date_commande', )
+
+
+
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category, AdminCategorie)
+admin.site.register(Commande, AdminCommande)
+
+# Register your models here.
 
 # Register your models here.
--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,18 @@
+from operator import index
 from django.urls import path
 from . import views
 from django.db import models
 
 
 urlpatterns = [
+    path('',views.index, name='home'),
+    path('detail', views.detail, name="detail"),
+    path('checkout', views.checkout, name="checkout"),
+    path('confirmation', views.confimation, name="confirmation" ),
+    
     path('register/' ,views.registre, name="register"),
     path('login/' ,views.Userlogin, name="login"),
     path('logout/' ,views.Userlogout, name="logout"),
-   
 ]
 
     
