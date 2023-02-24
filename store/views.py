f7b23cb96eae1a6b476c503aaaa9b2ffd34daa9c
neziha <neziha@gmail.com>
2023-02-24 15:53:17 +0000

    samina mina eh eh waka waka

--- store/views.py
+++ store/views.py
@@ -3,14 +3,18 @@ from multiprocessing import context
 from tkinter.tix import Form
 from django import forms
 from django.shortcuts import redirect, render
+
+#lile user
 from django.contrib.auth.forms import UserCreationForm
-
-
-
 from .forms import CreateNewUser
 from django.contrib import messages
 from django.contrib.auth import authenticate  ,logout
 from django.contrib.auth import login as mylogin
+
+#lile models
+from .models import Category, Product, Commande
+from django.core.paginator import Paginator
+from django.http import HttpResponse
 
 def login(request):
     context = {}
@@ -55,6 +59,50 @@ def Userlogout(request):
     return redirect('login')
   
        
+# l7oubi nedaniiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+
+
+def index(request):
+    product_object = Product.objects.all()
+    item_name = request.GET.get('item-name')
+    if item_name !='' and item_name is not None:
+        product_object = Product.objects.filter(title__icontains=item_name)
+    paginator = Paginator(product_object, 4)
+    page = request.GET.get('page')
+    product_object = paginator.get_page(page)
+    return render(request, 'shop/index.html', {'product_object': product_object})
+
+
+def detail(request, myid):
+    product_object = Product.objects.get(id=myid)
+    return render(request, 'shop/detail.html', {'product': product_object}) 
+
+def checkout(request):
+    if request.method == "POST":
+        items = request.POST.get('items')
+        total = request.POST.get('total')
+        nom = request.POST.get('nom')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        ville = request.POST.get('ville')
+        pays = request.POST.get('pays')
+        zipcode= request.POST.get('zipcode')
+        com = Commande(items=items,total=total, nom=nom, email=email, address=address, ville=ville, pays=pays, zipcode=zipcode)
+        com.save()
+        return redirect('confirmation')
+
+
+    return render(request, 'shop/checkout.html') 
+
+def confimation(request):
+    info = Commande.objects.all()[:1]
+    for item in info:
+        nom = item.nom
+    return render(request, 'shop/confirmation.html', {'name': nom})          
+
+
+
+
 
     
 # Create your views here.
